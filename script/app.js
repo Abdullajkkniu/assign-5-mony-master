d9d9b0167687808f46ab7a50ea664933e5e9a98f
MD Abdul Alim <abdulalimjkkniu@gmail.com>
2022-02-17 20:22:53 +0600

    added errors

--- script/app.js
+++ script/app.js
@@ -1,60 +1,55 @@
+// function start
 function updateBalance() {
     const incomeAmount = document.getElementById('income-amount');
-    let incomeValue = incomeAmount.value;
-    
-}
-function anotherFunction(){
-    const incomeAmount = document.getElementById('income-amount');
-    let incomeValue = incomeAmount.value;
-const foodAmount = document.getElementById('food-amount');
-    let foodValue = foodAmount.value;
+    let incomeValue = parseInt(incomeAmount.value);
+    const foodAmount = document.getElementById('food-amount');
+    let foodValue = parseInt(foodAmount.value);
     const rentAmount = document.getElementById('rent-amount');
-    let rentValue = rentAmount.value;
+    let rentValue = parseInt(rentAmount.value);
     const clothAmount = document.getElementById('cloth-amount');
-    let clothValue = clothAmount.value;
+    let clothValue = parseInt(clothAmount.value);
     const totalExpAmount = document.getElementById('totalExp-amount');
     const totalCost = totalExpAmount.innerText = parseInt(foodValue) + parseInt(rentValue) + parseInt(clothValue);
-    const balanceAmount = document.getElementById('balance-amount');
+    // const balanceAmount = document.getElementById('balance-amount');
 
-    let newBalance = balanceAmount.innerText = incomeValue - totalCost;
+    // let newBalance = balanceAmount.innerText = incomeValue - totalCost;
 
-    const saveParcentage = document.getElementById('save-percentage');
-    let saveValue = saveParcentage.value;
-    const savingAmount = document.getElementById('saving-amount');
-    let newSaveValue = savingAmount.innerText = incomeValue * (saveValue / 100);
-    const remainAmount = document.getElementById('remain-amount');
-    let remainText = remainAmount.innerText;
-    remainAmount.innerText = newBalance-newSaveValue;
-    const mainerrors = document.getElementById('main-error')
-    const savingAmountError = document.getElementById('savingAmont-error');
-    if(incomeValue<0 || foodValue<0 || rentValue<0 || clothValue<0 ||totalCost>incomeValue){
-        mainerrors.style.display="block";
-        incomeAmount.value='0';
-        foodAmount.value='0';
-        rentAmount.value='0';
-        clothAmount.value='0';
-        remainAmount.innerText = '0';
-        totalExpAmount.innerText='0';
-        balanceAmount.innerText = '0';
+    // const saveParcentage = document.getElementById('save-percentage');
+    // let saveValue = parseInt(saveParcentage.value);
+    // const savingAmount = document.getElementById('saving-amount');
+    // let newSaveValue = savingAmount.innerText = incomeValue * (saveValue / 100);
+    // const remainAmount = document.getElementById('remain-amount');
+    // remainAmount.innerText = newBalance - newSaveValue;
+    // const savingAmountError = document.getElementById('savingAmont-error');
+    
+    if(isNaN(foodValue) || isNaN(clothValue) || isNaN(rentValue) || isNaN(incomeValue)){
+        remainAmount.innerText = '';
+        totalExpAmount.innerText = '';
+        balanceAmount.innerText = '';
+        saveParcentage.value = '';
+        savingAmount.innerText='';
+        alert('Please enter a valid number');
+        }
+    else if(incomeValue < 0 || foodValue < 0 || rentValue < 0 || clothValue < 0 || newSaveValue < 0 || totalCost > incomeValue){
+        alert('Please enter a valid number');
+        
     }
-    else if(newSaveValue>newBalance){
-       savingAmountError.style.display = 'block';
-       savingAmount.innerText = '';
-       remainAmount.innerText = '';
+    else if (newSaveValue > newBalance) {
+        savingAmountError.style.display = 'block';
+        savingAmount.innerText = '';
+        remainAmount.innerText = '';
 
     }
-    else{
+    else {
         savingAmount.innerText = incomeValue * (saveValue / 100);
         savingAmountError.style.display = 'none';
     }
-
 }
-// ekhane 1ta function likhe uporer function a call kore dilei hobe
-
+// event listener start
 document.getElementById('calculate-amount').addEventListener('click', function () {
-    anotherFunction();
+    updateBalance();
 })
 document.getElementById('save-income').addEventListener('click', function () {
-    anotherFunction()
-    
+    updateBalance()
+
 })
